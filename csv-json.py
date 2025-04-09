fbb8d77d5e1a6690b6d7ee9b27c5ac37e0ea9f6c
Sidharth Sampathi Rao <sidharth.rao@quickheal.com>
2025-04-09 17:56:48 +0530

    changes

---
+++ csv-json.py
@@ -0,0 +1,27 @@
+import csv
+import datetime
+import json
+
+import dateutil.utils
+
+
+def csv_to_json(input_file, output_file):
+    # Initialize an empty list to store the data
+    data = []
+
+    # Read the CSV file and populate the data list
+    with open(input_file, 'r', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data.append(row)
+
+    # Write the data to a JSON file
+    with open(output_file, 'w') as jsonfile:
+        json.dump(data, jsonfile, indent=4)
+
+# Replace 'input_file.csv' with the path to your CSV file
+input_file = ('GCAF25C1-IN-9SW-T22 [09 Apr].csv')
+# Replace 'output_file.json' with the desired path for your output JSON file
+output_file = f'{1}.json'
+
+csv_to_json(input_file, output_file)
